1e6e5505290243b737d6bee23a705eac96a80645
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-16 07:23:48 +0200

    initial commit

---
+++ login/app.py
@@ -0,0 +1,81 @@
+from flask import Flask,render_template,request,url_for,redirect,session
+
+import pymongo
+import bcrypt
+
+app=Flask(__name__)
+app.secret_key="testing"
+client=pymongo.MongoClient("mongodb:localhost:27017/testing")
+db=client.get_database("testing")
+records=db.register
+
+@app.route("/",methods=['post','get'])
+def index():
+    message==''
+
+    if "email" in session:
+        return redirect(url_for("logged_in")
+
+    if request.method=="POST":
+        user=request.form.get("fullname")
+        email=request.form.get("email")
+        password1=request.form.get("password1")
+        password2=request.form.get("password2")
+
+        user_found=records.find_one({"name":user})
+        email_found=records.find_one({"email":email})
+
+        if user_found:
+            message="User already exists"
+            return render_template('index.html',message=message)
+        if email_found:
+            message="Email already in use"
+            return render_template('index.html',message=message)
+        if password1!=password2:
+            message="passwords  should match"
+            return render_template('index.html',message=message)
+        else:
+        hashed=bcrypt.hashpw(password2.encode('utf-8'),bcrypt.gensalt())
+        user_input={'name':user,'email':email,'password':hashed}
+        records.insert_one(user_input)
+    return render_template('index.html')
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    message="Please login to your account"
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method=="POST":
+        email=request.form.get("email")
+        password=request.form.get("password")
+
+
+        email_found=records.find_one({"email":email})
+        if email_found:
+            email_val=email_found['email']
+            passwordcheck=email_found['password']
+
+            if bcrypt.checkpw(password.encode('utf-8'),passwordcheck):
+                session["email"]=email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return  redirect(url_for("logged_in"))
+                message='Wrong password'
+                return render_template('login.html',message=message)
+        else:
+            message='Email not found'
+            return render_template('login.html',message=message)
+    return render_template('login.html',message=message)
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email=session['email']
+        return render_template('logged_in.html',email=email)
+    else:
+    return render_template('index.html')
+
+if __name__=="__main__":
+app.run(debug=True)
